7fd3e5c001390e426776967e7cafc9f803a9987a
OsmarVillegas <99563455+OsmarVillegas@users.noreply.github.com>
2023-03-10 19:04:23 -0600

    Frontend

--- src/component/formulario2.jsx
+++ src/component/formulario2.jsx
@@ -3,6 +3,7 @@ import "../Style/form2.css";
 import Table from "react-bootstrap/Table";
 
 export function Formulario() {
+  const [idModificar, setIdModificar] = useState(null);
 
   const [rows, setRows] = useState([
     {
@@ -21,7 +22,7 @@ export function Formulario() {
       description:
         "Genera Meeseeks y pidelo lo que quieras, pero no seas avaricioso o te podria costar la cabeza",
       images:
-        "https://static.wikia.nocookie.net/rick-y-morty-espanol/images/8/82/Caja_de_Meeseeks.png/revision/latest/scale-to-width-down/250?cb=20180211022717&path-prefix=es",
+        "https://static.wikia.nocookie.net/rick-y-morty-espanol/images/8/82/Caja_de_Meeseeks.png/revision/latest?cb=20180211022717&path-prefix=es",
     },
     {
       id: 3,
@@ -30,44 +31,30 @@ export function Formulario() {
       description:
         "Solo los verdaderos cerebros galaxia saben que hacer con estas semillas",
       images:
-        "https://static.wikia.nocookie.net/rick-y-morty-espanol/images/e/e3/Mega_Semillas.png/revision/latest/scale-to-width-down/1000?cb=20190329201247&path-prefix=es",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAm6-GwLnCENaXTb1Q5PO-tR54MI9oe8bM1Q&usqp=CAU",
     },
   ]);
 
-  const [formValues, setFormValues] = useState({ id: 0, name: "", description: "", price: 0, images: "" });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormValues((prevState) => ({ ...prevState, [name]: value }));
-  };
+  const [formValues, setFormValues] = useState({
+    id: 0,
+    name: "",
+    description: "",
+    price: 0,
+    images: "",
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setRows((prevState) => [...prevState, formValues]);
-    setFormValues({ id: 0, name: "", description: "", price: 0, images: "" });
-
-    if (modoEdicion) {
-      const nuevosDatos = datos.map((dato) => {
-        if (dato.id === id) {
-          return { id, nombre, descripcion, url };
-        } else {
-          return dato;
-        }
-      });
-      setDatos(nuevosDatos);
-      setId('');
-      setNombre('');
-      setDescripcion('');
-      setUrl('');
-      setModoEdicion(false);
+    if (idModificar !== null) {
+      const indexModificar = rows.findIndex((row) => row.id === idModificar);
+      const nuevosDatos = [...rows];
+      nuevosDatos[indexModificar] = formValues;
+      setRows(nuevosDatos);
+      setIdModificar(null);
     } else {
-      const nuevoDato = { id: Math.random().toString(36).substr(2, 9), nombre, descripcion, url };
-      setDatos([...datos, nuevoDato]);
-      setNombre('');
-      setDescripcion('');
-      setUrl('');
+      setRows((prevState) => [...prevState, formValues]);
     }
-
+    setFormValues({ id: 0, name: "", description: "", price: 0, images: "" });
   };
 
   const eliminarDato = (id) => {
@@ -75,13 +62,11 @@ export function Formulario() {
     setRows(nuevosDatos);
   };
 
-
   return (
     <div class="Formulario contenido">
       <div class="form">
         <h1 class="form-titulo">Formulario</h1>
         <div class="formulario">
-
           <Table className="form-table" striped bordered hover>
             <thead>
               <tr>
@@ -103,18 +88,23 @@ export function Formulario() {
                     <a href={row.images}>{row.images}</a>
                   </td>
                   <td>
-                    <button className="icon-pen" >
-                      <i class="fa-regular fa-pen-to-square"></i>
+                    <button
+                      className="icon-pen"
+                      onClick={() => setIdModificar(row.id)}
+                    >
+                      <i className="fa-regular fa-pen-to-square"></i>
                     </button>
                   </td>
                   <td>
-                    <button className="icon-trash" onClick={() => eliminarDato(row.id)}>
-                      <i class="fa-regular fa-trash"></i>
+                    <button
+                      className="icon-trash"
+                      onClick={() => eliminarDato(row.id)}
+                    >
+                      <i className="fa-regular fa-trash"></i>
                     </button>
                   </td>
                 </tr>
               ))}
-
             </tbody>
           </Table>
           <form onSubmit={handleSubmit}>
@@ -122,104 +112,85 @@ export function Formulario() {
               type="number"
               name="id"
               placeholder="Id"
-              value={formValues.id}
-              onChange={handleInputChange}
+              value={idModificar !== null ? idModificar : formValues.id}
+              disabled={idModificar !== null}
+              onChange={(e) => {
+                setFormValues((prevState) => ({
+                  ...prevState,
+                  id: parseInt(e.target.value),
+                }));
+              }}
             />
             <input
               type="text"
               name="name"
               placeholder="name"
-              value={formValues.name}
-              onChange={handleInputChange}
+              value={
+                idModificar !== null
+                  ? rows.find((row) => row.id === idModificar).name
+                  : formValues.name
+              }
+              onChange={(e) => {
+                setFormValues((prevState) => ({
+                  ...prevState,
+                  name: e.target.value,
+                }));
+              }}
             />
             <input
               type="text"
               name="price"
               placeholder="Precio"
-              value={formValues.price}
-              onChange={handleInputChange}
+              value={
+                idModificar !== null
+                  ? rows.find((row) => row.id === idModificar).price
+                  : formValues.price
+              }
+              onChange={(e) => {
+                setFormValues((prevState) => ({
+                  ...prevState,
+                  price: e.target.value,
+                }));
+              }}
             />
             <input
               type="text"
               name="description"
               placeholder="Description"
-              value={formValues.description}
-              onChange={handleInputChange}
+              value={
+                idModificar !== null
+                  ? rows.find((row) => row.id === idModificar).description
+                  : formValues.description
+              }
+              onChange={(e) => {
+                setFormValues((prevState) => ({
+                  ...prevState,
+                  description: e.target.value,
+                }));
+              }}
             />
             <input
               type="text"
               name="images"
               placeholder="URL"
-              value={formValues.images}
-              onChange={handleInputChange}
+              value={
+                idModificar !== null
+                  ? rows.find((row) => row.id === idModificar).images
+                  : formValues.images
+              }
+              onChange={(e) => {
+                setFormValues((prevState) => ({
+                  ...prevState,
+                  images: e.target.value,
+                }));
+              }}
             />
-            <button type="submit">Add row</button>
-            <button onClick={() => setModoEdicion(false)}>Cancelar</button>
+            <button type="submit">
+              {idModificar !== null ? "Modificar" : "Agregar"}
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-// export function Formulario() {
-//   const [rows, setRows] = useState([
-//     { name: "John", age: 25 },
-//     { name: "Sarah", age: 30 },
-//     { name: "Mark", age: 35 },
-//   ]);
-
-//   const [formValues, setFormValues] = useState({ name: "", age: "" });
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormValues((prevState) => ({ ...prevState, [name]: value }));
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     setRows((prevState) => [...prevState, formValues]);
-//     setFormValues({ name: "", age: "" });
-//   };
-
-//   return (
-//     <div>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Age</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {rows.map((row, index) => (
-//             <tr key={index}>
-//               <td>{row.name}</td>
-//               <td>{row.age}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Name"
-//           value={formValues.name}
-//           onChange={handleInputChange}
-//         />
-//         <input
-//           type="number"
-//           name="age"
-//           placeholder="Age"
-//           value={formValues.age}
-//           onChange={handleInputChange}
-//         />
-//         <button type="submit">Add row</button>
-//       </form>
-//     </div>
-//   );
-// };
-
